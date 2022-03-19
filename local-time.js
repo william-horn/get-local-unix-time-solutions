37f0fc77a3c93f26e61fb66a887684227283b533
William Horn <98601452+william-horn@users.noreply.github.com>
2022-03-19 15:40:27 -0400

    Update local-time.js

--- local-time.js
+++ local-time.js
@@ -38,3 +38,34 @@ setInterval(() => {
     console.log("Carl solution: \t\t\t", carlSolution(now));
     console.log("---------------------------------------------------");
 }, 10);
+
+
+
+// TIME FORMATTING
+// !! important =>
+// the code below can be replaced with 'new Date().toLocaleString'
+
+// generate mathematical sequence of {12, 1, 2, ..., 12, 1, 2, ...}
+function getClockHour(index) {
+    const time = (index + 11)%12 + 1;
+    const postfix = ~~(index/12)%2 == 0 ? "am" : "pm";
+    return { value: time, timePostfix: postfix }
+}
+
+// takes string of form "\d?\d?" and returns replacement string of zeros for unmatched digits
+// TODO: try to generalize this later into a 'formatNthPlace'?
+function formatTensPlace(numStr) {
+    numStr = numStr.toString();
+    return "00".replace(new RegExp("0{" + numStr.length + "}$"), numStr);
+}
+
+// return string in formatted time: hours:minutes:seconds(postfix)
+function getClockTime(date) {
+    const totalHours = date.getHours();
+    const clockHour = getClockHour(totalHours);
+    return clockHour.value
+        + ":" + formatTensPlace(date.getMinutes()) // format: "0\d" or "\d\d"
+        + ":" + formatTensPlace(date.getSeconds()) // format: "0\d" or "\d\d"
+        + clockHour.timePostfix;
+}
+// !!
